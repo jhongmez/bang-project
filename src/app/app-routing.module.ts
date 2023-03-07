82cc7191b3561a080e6db01db43dcf67c1e75b8f
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-03-06 22:50:16 -0500

    New components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ 
+		path: 'inicio', 
+		loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) 
+	},
+	{ 
+		path: 'login', 
+		loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule) 
+	},
+	{ path: 'administracion', loadChildren: () => import('./components/pages/admin/admin.module').then(m => m.AdminModule) }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
